07608ffd8dfa70ee152675a3f1c0dbb37bd9c0ca
Christianib003 <christianib003@gmail.com>
2023-11-06 23:16:47 -0800

    create function to remove c

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def no_c(my_string):
+    new_string = ""
+
+    for char in my_string:
+        if char != "c" and char != "C":
+            new_string += char
+
+    return new_string
