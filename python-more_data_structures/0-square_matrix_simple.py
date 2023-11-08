77b1e5aba1c1dff4de920ff4495a636ff49a69d4
Christian <christianib003@gmail.com>
2023-11-08 15:28:29 +0200

    create and import square_matrix_simple

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for mat in matrix:
+        matr = []
+        i = 0
+        while i < len(mat):
+            matr.append(mat[i] ** 2)
+            i += 1
+        new_matrix.append(matr)
+    return new_matrix
