d8ec4f0fd087e6c06199d3bdc82b1be10b781f23
Christian <christianib003@gmail.com>
2023-11-08 15:58:44 +0200

    create new list instead of modifying the existing one

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,9 +2,10 @@
 
 
 def search_replace(my_list, search, replace):
+    new_list = my_list[:]
     i = 0
-    while i < len(my_list):
-        if my_list[i] == search:
-            my_list[i] = replace
+    while i < len(new_list):
+        if new_list[i] == search:
+            new_list[i] = replace
         i += 1
-    return my_list
+    return new_list
