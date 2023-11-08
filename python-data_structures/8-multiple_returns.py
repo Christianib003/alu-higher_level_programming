b716b8904269bce106e6d43116fbb8999f65e2be
Christian <christianib003@gmail.com>
2023-11-08 11:38:20 +0200

    correct functionality when sentence is empty

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -4,5 +4,7 @@
 def multiple_returns(sentence):
   if not sentence:
     first_character = "None"
+  else:
+    first_character = sentence[0]
   
-  return ((len(sentence), sentence[0]))
+  return ((len(sentence), first_character))
