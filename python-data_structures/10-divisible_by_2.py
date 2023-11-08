4f235bc3fd5197d17f025eb76426bd8781612617
Christian <christianib003@gmail.com>
2023-11-08 12:16:53 +0200

    create and import divisible_by_2 function

---
+++ python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def divisible_by_2(my_list=[]):
+    new_list = []
+    if not my_list:
+        return new_list
+    else:
+        for num in my_list:
+            if num % 2 == 0:
+                new_list.append(True)
+            else:
+                new_list.append(False)
+    return new_list
