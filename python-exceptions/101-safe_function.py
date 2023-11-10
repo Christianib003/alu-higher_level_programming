282d099a5a059c068a4b13e6a1571d03dca94ce6
Christian <christianib003@gmail.com>
2023-11-10 20:19:17 +0200

    create and import safe_function function

---
+++ python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def safe_function(fct, *args):
+    import sys
+    try:
+        return (fct(*args))
+    except Exception as error:
+        sys.stderr.write("Exception: {}\n".format(error))
+        return (None)
