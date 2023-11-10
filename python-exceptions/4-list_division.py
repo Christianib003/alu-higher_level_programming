6a1f9016f7db06b5fe3372b9baff9e4d4df1cc3c
Christian <christianib003@gmail.com>
2023-11-10 11:21:31 +0200

    create and import list_division function

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = [0]*list_length
+    for i in range(list_length):
+        quotient = 0
+        try:
+            quotient = (my_list_1[i] / my_list_2[i])
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            new_list[i] = quotient
+    return new_list
