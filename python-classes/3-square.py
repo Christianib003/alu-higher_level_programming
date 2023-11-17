1d4b54f85915cb5cac4b530b7f7ab49b0ab1e740
Christian <christianib003@gmail.com>
2023-11-17 07:52:34 +0200

    create a method to find the area of the square

---
+++ python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""create square class and a public method that returns its area"""
+
+
+class Square:
+    """Init method"""
+    def __init__(self, size=0):
+
+        # validations
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    """Calculate the area"""
+    def area(self):
+        return self.__size ** 2
