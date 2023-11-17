ef56bf12584f62a6c5501831be6cbeff8ca3e63d
Christian <christianib003@gmail.com>
2023-11-17 10:51:39 +0200

    correct validation in position setter method

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,20 +6,15 @@ class Square:
     """Instance constructor"""
     def __init__(self, size=0, position=(0, 0)):
         """Validate position"""
-        if type(position) is tuple and len(position) == 2:
-                if all(type(i) is int for i in position):
-                    if all(i >= 0 for i in position):
-                        self.__position = position
-        else:
-            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
 
         """Validate size"""
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        # if type(size) is not int:
+        #     raise TypeError("size must be an integer")
+        # elif size < 0:
+        #     raise ValueError("size must be >= 0")
+        # else:
+        self.__size = size
 
     """Get the value of private attribute size"""
     @property
@@ -43,10 +38,11 @@ class Square:
     """Set the value of position"""
     @position.setter
     def position(self, value):
-        if type(value) is not tuple or len(value) != 2:
-            if any((type(i) is not int) or (i < 0) for i in value):
-                raise TypeError("position must be a tuple of 2 positive integers")
-        self.__position = value
+        if type(value) is tuple and len(value) == 2:
+                if all((type(i) is int) and (i >= 0) for i in value):
+                    self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
 
     """Return the value of the area of the square"""
     def area(self):
