68de8850b7a15fb0bf7721c6478f04c3a1afd67d
Christian <christianib003@gmail.com>
2023-11-17 09:46:23 +0200

    create square class with position

---
+++ python-classes/6-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Instance constructor"""
+    def __init__(self, size=0, position=(0, 0)):
+
+        self.__position = position
+        """Validate size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    """Get the value of private attribute size"""
+    @property
+    def size(self):
+        return self.__size
+    
+    @property
+    def position(self):
+        return self.__position
+
+    """Set the value of private attribute size"""
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @position.setter
+    def position(self, position):
+        if len(position) != 2 or any(i < 0 for i in position):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = position
+
+    """Return the value of the area of the square"""
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size > 0:
+            for y in range(self.__position[1]):
+                print()
+            for column in range(self.__size):
+                for x in range(self.__position[0]):
+                    print(" ", end="")
+                for row in range(self.__size):
+                    print("#", end="")
+                print()
+        else:
+            print()
