956ace17050a71d4c6266a6b993971ab4cdfbce6
Christian <christianib003@gmail.com>
2023-11-17 08:53:26 +0200

    create square class with my_print function

---
+++ python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """Instance constructor"""
+    def __init__(self, size=0):
+
+        """Validate size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    """Get the value of private attribute size"""
+    @property
+    def size(self):
+        return self.__size
+
+    """Set the value of private attribute size"""
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    """Return the value of the area of the square"""
+    def area(self):
+        return self.__size ** 2
+
+    """Print the square using #"""
+    def my_print(self):
+        i = 1
+        while i <= self.__size:
+            j = 1
+            while j <= self.__size:
+                if j == self.__size:
+                    print("#")
+                else:
+                    print("#", end="")
+                j += 1
+            i += 1
