878f9c069ff6c23a4295555d8d3f10ccdde78b5b
Christian <christianib003@gmail.com>
2023-11-20 08:04:47 +0200

    correct order of property printing

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -5,8 +5,8 @@
 class Rectangle:
     """this is an empty class"""
     def __init__(self, width=0, height=0):
-        self.__width = width
         self.__height = height
+        self.__width = width
 
     @property
     def width(self):
