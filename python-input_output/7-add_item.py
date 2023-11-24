137d985e8451bf3ae53af6929a8dcbbdf420ff2a
Christian <christianib003@gmail.com>
2023-11-24 17:37:20 +0200

    add documentation

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """script to add all arguments to a Python list and save to a file"""
 
+
 from sys import argv
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
