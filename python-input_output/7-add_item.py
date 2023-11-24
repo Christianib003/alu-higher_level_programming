07e14023432608df73020706e6713c174e3901c3
Christian <christianib003@gmail.com>
2023-11-24 17:41:04 +0200

    add documentation

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,14 +4,22 @@
 
 from sys import argv
 
+"""save to json file"""
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
+"""try except block"""
 try:
+    """ what to execute if the there is no exception"""
     arglist = load_from_json_file("add_item.json")
+
 except FileNotFoundError:
+    """what to execute if there is an error"""
+
     arglist = []
 
+""" final statements"""
 arglist += argv[1:]
 save_to_json_file(arglist, "add_item.json")
