269d95c960d887ba948a6d7c2a2b62e8cc49cc87
Christian <christianib003@gmail.com>
2023-11-24 17:42:38 +0200

    add further documentation

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
-"""script to add all arguments to a Python list and save to a file"""
+"""
+adds all arguments to a Python list, and then save them to a file
+"""
+import sys
 
 
-from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-"""save to json file"""
-
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-
-"""try except block"""
+open("add_item.json", "a")
 try:
-    """ what to execute if the there is no exception"""
-    arglist = load_from_json_file("add_item.json")
-
-except FileNotFoundError:
-    """what to execute if there is an error"""
-
-    arglist = []
-
-""" final statements"""
-arglist += argv[1:]
-save_to_json_file(arglist, "add_item.json")
+    load = load_from_json_file("add_item.json")
+except ValueError:
+    load = []
+save_to_json_file(load + sys.argv[1:], "add_item.json")
