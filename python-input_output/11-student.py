6a7301c3e6372bc46c825eb9f8f8c10eb786a912
Christian <christianib003@gmail.com>
2023-11-24 17:34:07 +0200

    add student class

---
+++ python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""defines class Student"""
+
+
+class Student():
+    """class to define student"""
+
+    def __init__(self, first_name, last_name, age):
+        """initializes student instances"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """returns dictionary descript for JSON serialization"""
+        if type(attrs) is list:
+            new_dict = {}
+            for key in self.__dict__:
+                for key2 in attrs:
+                    if key == key2:
+                        new_dict[key] = self.__dict__[key]
+            return new_dict
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        for key in json:
+            if key == "first_name":
+                self.first_name = json[key]
+            if key == "last_name":
+                self.last_name = json[key]
+            if key == "age":
+                self.age = json[key]
