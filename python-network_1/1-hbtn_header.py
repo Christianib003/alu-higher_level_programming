c6ca4050c9afb1b4e8f5709ba795551a2f01b401
Christian <christianib003@gmail.com>
2023-12-07 09:31:24 +0200

    correct errors

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,14 +1,10 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
-import urllib.request
-import sys
-
-
+"""
+Python script to send request to given URL and display value of X-Request-Id
+"""
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+    from urllib import request
+    from sys import argv
+    if argv[1]:
+        with request.urlopen(argv[1]) as response:
+            print(response.getheader('X-Request-Id'))
