ee4462f87832ceb652d8611384d061f8d7f96108
Christian <christianib003@gmail.com>
2023-12-07 12:05:45 +0200

    correct indentation

--- python-object_relational_mapping/102-relationship_cities_states_list.py
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -11,12 +11,12 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-            argv[1], argv[2], argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        for instance in session.query(City).order_by(City.id):
-                print("{}: {}".format(instance.id, instance.name), end="")
-                print(" -> {}".format(instance.state.name))
-        session.close()
+	engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+		argv[1], argv[2], argv[3]), pool_pre_ping=True)
+	Base.metadata.create_all(engine)
+	Session = sessionmaker(bind=engine)
+	session = Session()
+	for instance in session.query(City).order_by(City.id):
+		print("{}: {}".format(instance.id, instance.name), end="")
+		print(" -> {}".format(instance.state.name))
+	session.close()
